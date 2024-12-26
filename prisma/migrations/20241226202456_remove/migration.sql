c80fabe439afc84b7ff1627c93eabd5ef0ea4843
Erick Araújo <araujoerick.dev@gmail.com>
2024-12-26 18:03:50 -0300

    ✨ feat: add new migration and update prisma client

---
+++ prisma/migrations/20241226202456_remove/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `goal` on the `WaterHydration` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "WaterHydration" DROP COLUMN "goal";
